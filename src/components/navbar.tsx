e837c05d8172bfb1e579ad2f1d5e9ab4bc2a10e9
Victor Huynh <vichuynh6@gmail.com>
2023-04-22 14:23:05 +0000

    used next/link instead of <a> elements

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,43 +1,45 @@
+import Link from "next/link";
+
 export const NavBar = () => {
   return (
     <div className="fixed top-0 w-full">
       <nav className="flex h-8 space-x-2">
-        <a
+        <Link
           className="w-20 border-2 border-solid border-slate-300 text-center hover:bg-slate-200"
           href="/"
         >
           Home
-        </a>
-        <a
+        </Link>
+        <Link
           className="w-20 border-2 border-solid border-slate-300 text-center hover:bg-slate-200"
           href="/"
         >
           Options
-        </a>
-        <a
+        </Link>
+        <Link
           className="w-20 border-2 border-solid border-slate-300 text-center hover:bg-slate-200"
           href="/"
         >
           Profile
-        </a>
-        <a
+        </Link>
+        <Link
           className="w-20 border-2 border-solid border-slate-300 text-center hover:bg-slate-200"
           href="/"
         >
           Lobbies
-        </a>
-        <a
+        </Link>
+        <Link
           className="w-20 border-2 border-solid border-slate-300 text-center hover:bg-slate-200"
           href="/"
         >
           Create
-        </a>
-        <a
+        </Link>
+        <Link
           className="w-20 border-2 border-solid border-slate-300 text-center hover:bg-slate-200"
           href="/play"
         >
           Play
-        </a>
+        </Link>
       </nav>
     </div>
   );
