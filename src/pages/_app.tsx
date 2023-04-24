273998a7da3b4f76240618f7cd6f9e2563cdb974
Victor Huynh <vichuynh6@gmail.com>
2023-04-23 21:53:07 -0400

    added favicon, removed Head component from pages, fixed up default description and meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,16 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps} >
+      <Head>
+        <title>VVKH - Liar&apos;s Dice App</title>
+        <meta name="description" content="VVKH - Liar's Dice App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </ClerkProvider>
   );
